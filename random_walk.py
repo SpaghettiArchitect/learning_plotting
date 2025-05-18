cdae3e44630d4d4302b46307b99dc4898615e658
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-17 23:27:38 -0600

    Add starting class for random walks

---
+++ random_walk.py
@@ -0,0 +1,10 @@
+class RandomWalk:
+    """A class to generate random walks."""
+
+    def __init__(self, num_points: int = 5000) -> None:
+        """Initialize attributes of a walk."""
+        self.num_points = num_points
+
+        # All walks start at (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
